4f061f92aad7b97045245cecd14c7cb09ac2e522
SalmaRagab <salmaahragab@gmail.com>
2021-11-24 13:49:42 +0200

    add chat room socket connection

--- src/ChatRoom/ChatRoom.js
+++ src/ChatRoom/ChatRoom.js
@@ -1,72 +1,90 @@
 import ChatMessage from "../ChatMessage/ChatMessage";
 import { Button } from "react-bootstrap";
 import "./ChatRoom.css";
+import { useEffect, useState, useRef } from "react";
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
 
 export default function ChatRoom() {
 
-    let chatMessages = [
-        {
-            userName: "joffrey",
-            dateTime: "10 Mar 2018",
-            message: "Brilliant"
-        },
-        {
-            userName: "NINJA",
-            dateTime: "10 Mar 2018 9:55",
-            message: "Great resource, thanks"
-        },
-        {
-            userName: "I am mister brilliant",
-            dateTime: "10 Mar 2018 10:10",
-            message: "THANKSSSS!!"
-        },
-        {
-            userName: "martin7",
-            dateTime: "10 Mar 2018 10:19",
-            message: "Thanks Peter"
-        },
-        {
-            userName: "martin6",
-            dateTime: "10 Mar 2018 10:19",
-            message: "Thanks Peter"
-        },
-        {
-            userName: "martin5",
-            dateTime: "10 Mar 2018 10:19",
-            message: "Thanks Peter"
-        },
-        {
-            userName: "martin4",
-            dateTime: "10 Mar 2018 10:19",
-            message: "Thanks Peter"
-        },
-        {
-            userName: "martin3",
-            dateTime: "10 Mar 2018 10:19",
-            message: "Thanks Peter"
-        },
-        {
-            userName: "martin2",
-            dateTime: "10 Mar 2018 10:19",
-            message: "Thanks Peter"
-        },
-        {
-            userName: "martin1",
-            dateTime: "10 Mar 2018 10:17",
-            message: "Thanks Peter"
-        },
-    ]
+    const userName = localStorage.getItem('userName');
+    var socket = new SockJS('http://localhost:8080/stomp-endpoint');
+
+    let stompClient = null;
+    stompClient = Stomp.over(socket);
+    const messagesEndRef = useRef(null)
+
+    const [message, setMessage] = useState("");
+    const [chatMessages, setChatMessages] = useState([]);
+    const [connectedStatus, setConnectedStatus] = useState(false);
+
+    useEffect(() => {
+        fetch('http://localhost:8080/messages')
+              .then(response => response.json())
+              .then(data => setChatMessages(data));
+      }, []);
+
+    useEffect(() => {
+        connect();
+    }, []);
+
+    function connect(e) {
+        stompClient.connect({}, function (frame) {
+            console.log('Connected: ' + frame);
+            setConnected(true);
+            stompClient.subscribe('/message/chat', function (message) {
+                console.log('MESSAGEEE: ', message);
+                onMessageReceived(message.body);
+            });
+        });
+    }
+
+    function sendMessage(e) {
+        e.preventDefault();
+        if (message.trim().length > 0) {
+            stompClient.send("/app/send-message", {}, JSON.stringify({'sender': userName, 'content': message}));
+        }
+        setMessage('');
+    }
+
+    function setConnected(connected) {
+        if (connected) {
+            setConnectedStatus(true);
+        } else {
+            setConnectedStatus(false);
+        }
+    }
+
+    function onMessageReceived(payload) {
+        var message = JSON.parse(payload);
+        setChatMessages(prevMessages => {
+            const auxArr = [...prevMessages];
+            auxArr.push(message);
+            return auxArr;
+        })
+    }
+
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+    
+    useEffect(scrollToBottom, [chatMessages]);
+
     return (
         <div className="chatRoom">
             <div className="messagesSection">
-                {chatMessages.map(chatMessage => (
-                    <ChatMessage userName={chatMessage.userName}
-                    dateTime={chatMessage.dateTime} message={chatMessage.message} />
+                {chatMessages.map((chatMessage, i) => (
+                    <ChatMessage key={i} sender={chatMessage.sender}
+                    date={chatMessage.dateTime} content={chatMessage.content} />
                 ))}
+                <div ref={messagesEndRef}/>
+                {!connectedStatus && <div>You're not connected yet</div>}
             </div>
-            <div class="myMessageSection">
-                <input className="messageInput" type="text"></input>
-                <Button className="sendMessageBtn" size="sm">Send</Button>
+            <div className="myMessageSection">
+                <input className="messageInput" value={message} onChange={(e) => setMessage(e.target.value)} type="text" 
+                onKeyPress={(e) => e.key === 'Enter' ? sendMessage(e) : null}
+                />
+                <Button className="sendMessageBtn" onClick={(e) => sendMessage(e)} size="sm">Send</Button>
             </div>
         </div>
     );
