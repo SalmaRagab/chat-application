72347e0b8e8f9a5efa9a83553457d543f8ce6a18
SalmaRagab <salmaahragab@gmail.com>
2021-11-24 21:23:27 +0200

    update styles

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
+import './Home.css';
 
 export default function Home() {
     return (
         <div className="Home">
-            Welcome!
-            <Link to="/login">Login from here</Link>
+            <div className="welcomeMessage">
+                Welcome to the Chat Room!
+            </div>
+            <div className="navigateToLink">
+                <Link to="/login">Press here to enter your name and have some fun!</Link>
+            </div>
         </div>
     );
 }
